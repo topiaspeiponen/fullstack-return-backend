89455528019bd185f932852cc785cee198072963
topiaspe <topiaspe@metropolia.fi>
2022-03-16 20:41:13 +0200

    exercise 3.4 phonebook_backend

--- index.js
+++ index.js
@@ -1,57 +1,74 @@
-const { response } = require('express')
-const express = require('express')
-const app = express()
+const { response } = require("express");
+const express = require("express");
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 let persons = [
-          {
-            "name": "Arto Hellas",
-            "number": "040-123456",
-            "id": 1
-          },
-          {
-            "name": "Ada Lovelace",
-            "number": "39-44-5323523",
-            "id": 2
-          },
-          {
-            "name": "Dan Abramov",
-            "number": "12-43-234345",
-            "id": 3
-          },
-          {
-            "name": "Marry Poppendick",
-            "number": "39-23-6423122",
-            "id": 4
-          }
-]
+  {
+    name: "Arto Hellas",
+    number: "040-123456",
+    id: 1,
+  },
+  {
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+    id: 2,
+  },
+  {
+    name: "Dan Abramov",
+    number: "12-43-234345",
+    id: 3,
+  },
+  {
+    name: "Marry Poppendick",
+    number: "39-23-6423122",
+    id: 4,
+  },
+];
 
-app.get('/api/persons', (req, res) => {
-  res.json(persons)
-})
+app.get("/api/persons", (req, res) => {
+  res.json(persons);
+});
 
-app.get('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    const person = persons.find(person => person.id === id)
+app.get("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.find((person) => person.id === id);
 
-    if (person) {
-        console.log(req.params, id)
-        res.json(person)
-    } else {
-        return res.status(204).json({
-            error: 'no content'
-        })
-    }
-  })
+  if (person) {
+    console.log(req.params, id, person);
+    res.json(person);
+  } else {
+    return res.status(404).json({
+      error: "not found"
+    });
+  }
+});
 
-app.get('/info', (req, res) => {
-    const timestamp = new Date();
-    const infoElement = `<p>Phonebook has info for ${persons.length} people</p><p>${timestamp}</p>`;
-    res.send(infoElement);
-  })
+app.delete("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.find((person) => person.id === id);
 
-const PORT = 3001
+  if (person) {
+    console.log(req.params, id);
+    const newPersons = persons;
+    persons = newPersons.filter(person => person.id !== id)
+    console.log(persons)
+    res.json(`${person.name} was deleted succesfully`);
+  } else {
+    return res.status(404).json({
+      error: "not found"
+    });
+  }
+});
+
+app.get("/info", (req, res) => {
+  const timestamp = new Date();
+  const infoElement = `<p>Phonebook has info for ${persons.length} people</p><p>${timestamp}</p>`;
+  res.send(infoElement);
+});
+
+const PORT = 3001;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
