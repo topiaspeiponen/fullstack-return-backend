d7043a665e059a31545d4c7353dc3f6dac20f9be
topiaspe <topiaspe@metropolia.fi>
2022-03-17 18:57:46 +0200

    exercise 3.7 and 3.8 phonebook_backend

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const { response } = require("express");
 const express = require("express");
+const morgan = require('morgan')
 const app = express();
 
 app.use(express.json());
+morgan.token('reqBody', function (req, res) { return JSON.stringify(req.body)})
+app.use(morgan(':method :url :status :res[content-length] - :response-time  ms :reqBody'))
+
 
 let persons = [
   {
