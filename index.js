d760006718c1a25f150b8f561bea02ec9bb145dc
topiaspe <topiaspe@metropolia.fi>
2022-03-20 19:17:38 +0200

    exercise 3.19 phonebook_backend

--- index.js
+++ index.js
@@ -1,39 +1,18 @@
 require('dotenv').config()
 const { response } = require("express");
 const express = require("express");
+const cors = require('cors')
 const morgan = require('morgan')
 const app = express();
 
 
 app.use(express.json());
+app.use(cors())
 morgan.token('reqBody', function (req, res) { return JSON.stringify(req.body)})
 app.use(morgan(':method :url :status :res[content-length] - :response-time  ms :reqBody'))
 app.use(express.static('build'))
 const Person = require('./models/person')
 
-/** 
-let persons = [
-  {
-    name: "Arto Hellas",
-    number: "040-123456",
-    id: 1,
-  },
-  {
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-    id: 2,
-  },
-  {
-    name: "Dan Abramov",
-    number: "12-43-234345",
-    id: 3,
-  },
-  {
-    name: "Marry Poppendick",
-    number: "39-23-6423122",
-    id: 4,
-  },
-];*/
 
 app.get("/api/persons", (req, res, next) => {
   Person.find({})
@@ -60,21 +39,13 @@ app.post("/api/persons", (req, res, next) => {
           });
     }
 
-    /*if (persons.some(person => person.name === body.name)) {
-        return res.status(406).json({
-            error: "name must be unique"
-          });
-    }*/
-
     const newPerson = new Person({
         name: body.name,
         number: body.number
     })
 
-    //const newPerson = {...initialPerson, id: Math.floor(Math.random() * 9000000) }
-
     newPerson.save()
-      .then(result => {
+      .then(() => {
         res.json(`${newPerson.name} was succesfully added`);
       })
       .catch(error => next(error))
@@ -116,9 +87,11 @@ app.get("/info", (req, res, next) => {
 });
 
 const errorHandler = (error, req, res, next) => {
-  console.error('error handler encountered error: ', error.message)
+  console.error('error handler encountered error: ', error.message, error.name, error.name === 'ValidationError')
   if (error.name === 'CastError') {
     return res.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return res.status(400).json({ error: error.message })
   } else {
     return res.status(400).send({ error: 'bad request'})
   }
