77502d0b616b5803db0a6eda86e619a9d5f710b1
topiaspe <topiaspe@metropolia.fi>
2022-03-20 19:33:16 +0200

    exercise 3.20 phonebook_backend

--- models/person.js
+++ models/person.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const url = process.env.MONGODB_URI;
 
 mongoose.connect(url)
-    .then(result => {
+    .then(() => {
         console.log('Connection to MongoDB established')
     })
     .catch(error => {
